39ca9ddb196868cc5ba19cc155b928bcf5a27ee9
Source <dean-91@mail.ru>
2020-08-07 14:02:29 +0300

    Познакомились с операторами

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 let myTrue = true;
 let myFalse = false;
 
-
-console.log(2 === 2);
-console.log(2 === 3);
-
-
-console.log(Boolean(5));
-console.log(Boolean('Hello'));
-console.log(Boolean({}));
-console.log(Boolean([]));
-console.log(Boolean(function () {}));
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean(null));
-console.log(Boolean(NaN));
-console.log(Boolean(''));
-
-if (5) {}
+// || &&  !
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);

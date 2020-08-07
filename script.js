dc3f2c15f751dcf627c611783d9979321f6fe828
Source <dean-91@mail.ru>
2020-08-07 15:25:28 +0300

    Познакомились со строками

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-console.log(Math.pow(5, 3));
-console.log(Number.isInteger(5.5));
+//let myString = 'Hello world!';
+//let myStr = "\t Hello \"my\" \n freinds!";
+
+//onsole.log('Hello "my" friends!');
+//console.log('myStr: ', myStr);
+
+
+//console.log(typeof (5 + '10'));
+//console.log('Hello' + ' ' + 'world');
+
+
+//let str = 'Hello my friends';
+//console.log(str.length);
+let str = 'apple, kiwi, orange'
+
+//console.log(str.toUpperCase());
+//console.log(str.toLowerCase());
+//console.log(str.charAt(0));
+//console.log(str.charAt(1));
+//console.log(str[0]);
+//console.log(str.substring(6));
+//console.log(str.substring(9, 15));
+//console.log(str.slice(6));
+//console.log(str.slice(-8));
+//console.log(str.substr(6, 9));
+//console.log(str.replace('my Friends', 'World'));
+console.log(str.split(', '));

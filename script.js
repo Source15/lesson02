7c4ac293417b7bf1e1f2089c37ce1efe0bf08723
Source <dean-91@mail.ru>
2020-08-07 13:05:34 +0300

    Познакомились с type of

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 let myVar;
 
 myVar = 10;
+console.log(typeof myVar);
 myVar = 'Hello world';
+console.log(typeof myVar);
 myVar = true;
+console.log(typeof myVar);
 myVar = null;
+console.log(typeof myVar);
 myVar = undefined;
+console.log(typeof myVar);
 myVar = Symbol();
+console.log(typeof myVar);
 
 myVar = {};
 
-console.log(typeof myVar);
 
-myVar = [];
-let regExp = /w+/g;
-let func = function () {};
-let error = Error('error message');
-let date = new Date();
+//let myArr = [];
+//let regExp = /w+/g;
+//let func = function () {};
+//let error = Error('error message');
+//let date = new Date();
+//console.log(typeof myArr);

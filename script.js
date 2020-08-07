c93bc225405ac2acf132189a01f1fce398fafbd0
Source <dean-91@mail.ru>
2020-08-07 12:55:33 +0300

    new lesson02

---
+++ script.js
@@ -0,0 +1,18 @@
+let myVar;
+
+myVar = 10;
+myVar = 'Hello world';
+myVar = true;
+myVar = null;
+myVar = undefined;
+myVar = Symbol();
+
+myVar = {};
+
+console.log(typeof myVar);
+
+myVar = [];
+let regExp = /w+/g;
+let func = function () {};
+let error = Error('error message');
+let date = new Date();

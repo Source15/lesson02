0e8039c8d9408b9441cc3ff3485153cdd84f98d8
Source <dean-91@mail.ru>
2020-08-07 19:00:15 +0300

    новые правки

---
+++ HomeWork.js
@@ -0,0 +1,23 @@
+let money = 15000;
+let income = 'freelance';
+let addExpenses = 'такси,связь,интернет';
+let deposit = true;
+let mission = 1000000;
+let period = 12;
+
+//Вводим значения типа данных
+console.log(typeof money);
+console.log(typeof freelance);
+console.log(typeof deposit);
+//длина строки
+let str = 'addExpenses';
+console.log(str.length);
+//конкотинируем строку
+console.log('period' + 'mission');
+//приводим строку к нижнему регистру
+console.log(addExpenses.toLowerCase());
+//выводим в массив
+console.log(addExpenses.split(''));
+//построить доход за месяц
+let budgetDay = 1000;
+console.log(1000 / 30);

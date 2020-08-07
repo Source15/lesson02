f1a0b02db0b51b3544e82b7b1c077dcbad5153ae
Source <dean-91@mail.ru>
2020-08-07 16:32:32 +0300

    ДЗ

---
+++ HomeWork.js
@@ -0,0 +1,21 @@
+let money = 150000;
+let income = 'freelance';
+let deposit = true;
+let mission = 100000;
+let period = 12;
+
+console.log(money);
+console.log(income);
+console.log(deposit);
+
+let str = 'addExpenses';
+console.log(str.length);
+
+console.log('Период равен' + ' 12' + ' месяцев ' + ' и' + ' Цель заработать' + ' 100000' + ' рублей');
+
+console.log(str.toLowerCase());
+let word = 'add, Expen, ses'
+console.log(word.split(', '));
+
+let budgetDay = (100 / 30)
+console.log(budgetDay);

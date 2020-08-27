9ac2b266cf6b0a3422aeb6301065cdf0f98e30ad
source <dean-91@mail.ru>
2020-08-27 19:44:30 +0300

    урок15тый часть2

---
+++ js/dop.js
@@ -0,0 +1,26 @@
+'use strict'
+
+class First {
+
+   hello() {
+      console.log("Привет я метод родителя!");
+
+   }
+
+};
+
+class Second extends First {
+
+   hello() {
+      First1.hello();
+
+      console.log("А я наследуемый метод!");
+
+   }
+
+};
+
+const First1 = new First();
+const Second1 = new Second();
+
+Second1.hello()
